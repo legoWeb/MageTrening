ad32ce0802c4bde8d68942187efbd4b206dfe487
poluskinoleg <olegpola87@gmail.com>
2020-02-23 21:13:52 +0200

    #2.5 create controller who redirect Magento_Cms pages

--- app/code/Training/Test/Controller/Index/Index.php
+++ app/code/Training/Test/Controller/Index/Index.php
@@ -1,13 +1,37 @@
 <?php
 
+namespace Training\Test\Controller\Index;
 
-namespace Training\Test\Controller;
-
-
+/**
+ * Class Index
+ * @package Training\Test\Controller\Index
+ */
 class Index extends \Magento\Framework\App\Action\Action
 {
+    /**
+     * @var \Magento\Framework\Controller\Result\RawFactory
+     */
+    private $resultRawFactory;
+
+    /**
+     * Index constructor.
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
+    ) {
+        $this->resultRawFactory = $resultRawFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\Result\Raw|\Magento\Framework\Controller\ResultInterface
+     */
     public function execute()
     {
-        $this->getResponse()->appendBody('simple text');
+        $resultRaw = $this->resultRawFactory->create();
+        return $resultRaw->setContents('simple text');
     }
 }
